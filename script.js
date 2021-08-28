05ed9d7942dd289a39b5b9066fa93f5fc923e937
47tim <89678768+47tim@users.noreply.github.com>
2021-08-27 22:45:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    var scrollLink = $('.scroll');
+
+    scrollLink.click(function(e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 800)
+    })
+})
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
