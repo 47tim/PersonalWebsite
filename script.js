796e9f5926bf4ddc8400a40e644e09d4d0a8608a
47tim <89678768+47tim@users.noreply.github.com>
2021-08-30 16:33:35 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,41 @@ $(document).ready(function() {
     })
 })
 
+/* FADE IN */ 
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -200px 0px"
+};
+
+const appearOnScroll = new  IntersectionObserver
+(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting){
+            return;
+        } 
+        else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+},
+appearOptions)
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
+
+
+/* NAV MENU */
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
